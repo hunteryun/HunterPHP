6485dc05ada5ababf3b8e220fec02b868f6f4d58
zhiqiang <aviggngyv@gmail.com>
2017-03-23 18:41:54 +0800

    add debug model provide a friendly error

--- core/lib/Hunter/App/Strategy/HunterStrategy.php
+++ core/lib/Hunter/App/Strategy/HunterStrategy.php
@@ -96,10 +96,81 @@ class HunterStrategy extends ApplicationStrategy implements StrategyInterface
      */
     public function getExceptionDecorator(Exception $exception)
     {
-        return function (ServerRequestInterface $request, ResponseInterface $response) use ($exception) {
-            $response->getBody()->write('Sorry, this page have a error : '.$exception->getMessage());
-            return $response;
+        global $hunter_debug;
+        return function (ServerRequestInterface $request, ResponseInterface $response) use ($exception, $hunter_debug) {
+          $msg = $exception->getMessage();
+
+          if (!$hunter_debug) {
+            echo 'This page have a error, please contact the administrator!';
+            exit;
+          }
+
+          header('HTTP/1.1 500 Internal Server Error');
+          header("status: 500 Internal Server Error");
+          $trace = $exception->getTrace();
+          $runTrace = $exception->getTrace();
+          krsort($runTrace);
+          $traceMessageHtml = null;
+          $sqlTraceHtml = '';
+          $k = 1;
+          foreach ($runTrace as $v) {
+           if(isset($v['file'])){
+             $traceMessageHtml.='<tr class="bg1"><td>'.$k.'</td><td>'.$v['file'].'</td><td>'.$v['line'].'</td><td>'.self::getLineCode($v['file'], $v['line']).'</td></tr>';
+           }
+           $k++;
+          }
+          unset($k);unset($trace);unset($runTrace);unset($trace);
+          $body = '
+          <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+          <html><head><title>'.$_SERVER['HTTP_HOST'].' - PHP Error</title>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="ROBOTS" content="NOINDEX,NOFOLLOW,NOARCHIVE" />
+          <style type="text/css">
+          <!--
+          body { background-color: white; color: black; font: 9pt/11pt verdana, arial, sans-serif;}
+          #container { width: 90%;margin-left:auto;margin-right:auto; }
+          #message   { width: 90%; color: black; }
+          .red  {color: red;}
+          a:link     { font: 9pt/11pt verdana, arial, sans-serif; color: red; }
+          a:visited  { font: 9pt/11pt verdana, arial, sans-serif; color: #4e4e4e; }
+          h1 { color: #FF0000; font: 18pt "Verdana"; margin-bottom: 0.5em;}
+          .bg1{ background-color: #FFFFCC;}
+          .bg2{ background-color: #EEEEEE;}
+          .table {background: #AAAAAA; font: 11pt Menlo,Consolas,"Lucida Console"}
+          .info {background: none repeat scroll 0 0 #F3F3F3;border: 0px solid #aaaaaa;border-radius: 10px 10px 10px 10px;color: #000000;font-size: 11pt;line-height: 160%;margin-bottom: 1em;padding: 1em;}
+          .help {
+          background: #F3F3F3;border-radius: 10px 10px 10px 10px;font: 12px verdana, arial, sans-serif;text-align: center;line-height: 160%;padding: 1em;}
+          .mind {
+          background: none repeat scroll 0 0 #FFFFCC;
+          border: 1px solid #aaaaaa;
+          color: #000000;
+          font: arial, sans-serif;
+          font-size: 9pt;
+          line-height: 160%;
+          margin-top: 1em;
+          padding: 4px;}
+          -->
+          </style></head><body><div id="container"><h1>HunterPHP DEBUG</h1><div class="info">(1146)'.$msg.'</div><div class="info"><p><strong>PHP Trace</strong></p><table cellpadding="5" cellspacing="1" width="100%" class="table"><tr class="bg2"><td style="width:2%">No.</td><td style="width:45%">File</td><td style="width:5%">Line</td><td style="width:48%">Code</td></tr>'.$traceMessageHtml.'</table><p><strong>SQL Query</strong></p><table cellpadding="5" cellspacing="1" width="100%" class="table"><tr class="bg2"><td style="width:2%">No.</td><td style="width:73%">SQL</td><td style="width:10%">Cost Time</td><td style="width:15%">Affected Rows</td></tr>'.$sqlTraceHtml.'</table></div> <div class="help"><a href="http://'.$_SERVER['HTTP_HOST'].'">'.$_SERVER['HTTP_HOST'].'</a> 已经将此出错信息详细记录, 由此给您带来的访问不便我们深感歉意.</div></div></body></html>';
+
+          $response->getBody()->write($body);
+          return $response;
         };
+    }
+
+    /**
+     * get line of code
+     */
+    private static function getLineCode($file,$line) {
+      $fp = fopen($file,'r');
+      $i = 0;
+      while(!feof($fp)) {
+        $i++;
+        $c = fgets($fp);
+        if($i==$line) {
+          return $c;
+          break;
+        }
+      }
     }
 
 }
